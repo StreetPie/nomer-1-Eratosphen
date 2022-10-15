3c189bfe73cf802aafdfa7773d8a39254d52de07
OparinDG <OparinDG@310D-9>
2022-10-15 11:37:22 +0700

    Добавьте файлы проекта.

---
+++ 5523455245252452/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//Решение Эратосфена
+Stopwatch stopwatch = new Stopwatch();
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("Решето Эратосфена");
+    Console.WriteLine("ВВедите число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    bool[] A = new bool[x];
+
+    stopwatch.Start();
+    for (int i = 2; i < Math.Sqrt(x) + 1; i++)
+    {
+        if (!A[i])
+        {
+            for (int j = i * i; j < x; j += i)
+            {
+                A[j] = true;
+            }
+        }
+    }
+    for (int i = 2; i < x; i++)
+    {
+        if (!A[i])
+        {
+            Console.WriteLine("{0}", i);
+        }
+    }
+    stopwatch.Stop();
+    Console.WriteLine("Время:" + stopwatch.ElapsedMilliseconds);
+}
